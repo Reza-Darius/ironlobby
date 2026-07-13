7018497058eb191db7b24c7b9188fc334ee7d66e
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-07-13 16:07:22 +0200

    feat: add AppHeader and AppFooter component

---
+++ frontend/app/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+    <header class="header">
+        App Header
+        
+        <nav class="header__nav">
+            <ul class="header__list">
+                <li class="header__item">
+                    <NuxtLink to="/">
+                        Home
+                    </NuxtLink>
+                </li>
+
+                <li class="header__item">
+                    <NuxtLink to="/users">
+                        Users
+                    </NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>

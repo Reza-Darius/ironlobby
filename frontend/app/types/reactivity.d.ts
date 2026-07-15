a4c512e3e661605f6c6de9a4b1e4066380dfb6a6
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-07-15 21:52:32 +0200

    feat: add reactivity types

---
+++ frontend/app/types/reactivity.d.ts
@@ -0,0 +1,16 @@
+import type {
+  CalendarDate,
+  CalendarDateTime,
+  Time,
+  ZonedDateTime,
+} from "@internationalized/date";
+
+declare module "@vue/reactivity" {
+  export interface RefUnwrapBailTypes {
+    internationalizedDate:
+      | CalendarDate
+      | CalendarDateTime
+      | Time
+      | ZonedDateTime;
+  }
+}
